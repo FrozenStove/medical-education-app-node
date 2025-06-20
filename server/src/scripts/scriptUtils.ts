fdd78f44e89a74cf934a6019505998501a2f70af
FrozenStove <Yessysutter@gmail.com>
2025-06-20 15:34:53 -0700

    unify script return object

--- server/src/scripts/scriptUtils.ts
+++ server/src/scripts/scriptUtils.ts
@@ -1,27 +1,11 @@
-import { ChromaCheckResult } from "./checkChroma";
-import { IngestResult } from "./ingestArticles";
-import { VerificationResult } from "./verifyIngestion";
 
 export type ScriptName = 'ingest' | 'verify' | 'check';
 
 export type ScriptResult = {
     scriptName: ScriptName;
-    result: string;
+    documentCount: number;
+    successfulFiles: number;
+    failedFiles: number;
+    data: any;
     error?: string;
-}
-
-
-export type IngestScriptResult = ScriptResult & {
-    scriptName: 'ingest';
-    result: IngestResult;
-}
-
-export type VerifyScriptResult = ScriptResult & {
-    scriptName: 'verify';
-    result: VerificationResult;
-}
-
-export type CheckScriptResult = ScriptResult & {
-    scriptName: 'check';
-    result: ChromaCheckResult;
 }
