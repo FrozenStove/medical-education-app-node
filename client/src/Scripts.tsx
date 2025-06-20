6b9a7eb12bdb275c9e1f300f98f134d4e1ca9eb6
FrozenStove <Yessysutter@gmail.com>
2025-06-20 15:47:18 -0700

    render script result

--- client/src/Scripts.tsx
+++ client/src/Scripts.tsx
@@ -2,13 +2,37 @@ import React, { useState } from 'react';
 import './Scripts.css';
 import { SERVER_URL } from './constants';
 
+type ScriptName = 'ingest' | 'verify' | 'check';
+
+type ScriptResult = {
+    scriptName: ScriptName;
+    documentCount: number;
+    successfulFiles: number;
+    failedFiles: number;
+    data: any;
+    error?: string;
+}
+
+
 function Scripts() {
     const [auth, setAuth] = useState('');
     const [result, setResult] = useState('');
+    const [data, setData] = useState<ScriptResult | null>(null);
+    let Result = (<div>
+        {data && (
+            <>
+                <p>Script Name: {data.scriptName}</p>
+                <p>Document Count: {data.documentCount}</p>
+                <p>Successful Files: {data.successfulFiles}</p>
+                <p>Failed Files: {data.failedFiles}</p>
+            </>
+        )}
+    </div>)
 
     const handleButtonClick = async (button: string) => {
         setResult('');
         console.log(`${SERVER_URL}/api/scripts/${button}`);
+        setData(null);
         try {
             const response = await fetch(`${SERVER_URL}/api/scripts/${button}`, {
                 method: 'GET',
@@ -16,11 +40,12 @@ function Scripts() {
                     'auth': auth
                 }
             });
-            const data = await response.json();
+            const data: ScriptResult = await response.json();
             if (data.error) {
                 setResult('Error: ' + data.error);
             } else {
-                setResult('Success: ' + data.message);
+                setData(data);
+                setResult('Success: ' + data.scriptName);
             }
         } catch (error) {
             console.log(error);
@@ -36,6 +61,7 @@ function Scripts() {
             <button onClick={() => handleButtonClick('verify')}>Verify</button>
             <button onClick={() => handleButtonClick('check')}>Check</button>
             <div className="result">{result}</div>
+            {Result}
         </div>
     );
 }
